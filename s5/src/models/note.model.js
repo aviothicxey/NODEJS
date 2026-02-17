8efe79d6edf95dfe237de271f56c202a03d814d7
Aparna <gargaparna78@gmail.com>
2026-02-17 20:05:17 +0530

    init

--- s5/src/models/note.model.js
+++ s5/src/models/note.model.js
@@ -8,6 +8,6 @@ description : String,
 
 })
 
-const noteModel = mongoose.model("note" , noteSchema);
+const noteModel = mongoose.model("note" , noteSchema); // first argument : name of the collection in database, second argument : schema which we want to use for that collection
 
 module.exports = noteModel;
