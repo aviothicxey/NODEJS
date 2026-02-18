81d8f5a4c68bfa5dcc42c7a4542e28ea37fa73da
Aparna <gargaparna78@gmail.com>
2026-02-19 00:20:41 +0530

    int

---
+++ Project_Practice/project01/src/services/storage.service.js
@@ -0,0 +1,19 @@
+const {imageKit} = require('@imagekit/nodejs');
+
+const imagekit = new imagekit({
+    PublicKey : process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndPoint : process.env.IMAGEKIT_MONGO_URI
+})
+
+async function uploadFile(buffer){
+    const result = await imagekit.files({
+        file:buffer.toString("base_64"),
+        fileName : "image.jpg",
+    })
+    return result.status(201).json({
+        message : "post created",
+    })
+}
+
+module.exports = uploadFile;
